dfdf3395c3a21c49aff45e2ac1db551805c2dd4a
macedo01 <pedromacedo113@hotmail.com>
2024-03-14 01:15:15 -0300

    progressing the front end

--- src/app/components/time-filter/time-filter.component.ts
+++ src/app/components/time-filter/time-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'time-filter',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class TimeFilterComponent {
 
+
+  times: Times[] = [{label: "Manhã", time:"06:00 ás 12:00"}, {label: "Tarde", time:"12:01 ás 18:00"}, 
+  {label: "Noite", time:"18:01 ás 23:00"}]
+
+
+  clearFilter(){
+    var radioElement = document.getElementsByName("labels");
+    radioElement.forEach((element) => {
+      console.log(element);
+      
+    });
+    
+    
+  }
+}
+
+export class Times {
+  label: string;
+  time: string;
+
+  constructor(label: string, time: string){
+    this.label = label;
+    this.time = time;
+  }
 }
